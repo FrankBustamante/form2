e188ad7b29180bcfc88863a8ef11ba20b5e27d8e
FrankBustamante <frankbustab@gmail.com>
2018-08-09 09:19:52 -0500

    initial

---
+++ form/app/src/main/java/unac/form/com/Form2Activity.java
@@ -0,0 +1,22 @@
+package unac.form.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class Form2Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form2);
+    }
+
+
+}
